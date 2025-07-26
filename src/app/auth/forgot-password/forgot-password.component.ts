1271fa6650fc210f36c942aaca8c6cd7cee08d1d
abrandhawa <alibakhat01@gmail.com>
2025-07-26 16:49:38 +0500

    revert code

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,40 +1,19 @@
-import { Component } from "@angular/core";
-import { routes } from "../../shared/routes/routes";
-import { Router, RouterLink } from "@angular/router";
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from "@angular/forms";
-import { CommonModule } from "@angular/common";
+import { Component } from '@angular/core';
+import { routes } from '../../shared/routes/routes';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: "app-forgot-password",
-  imports: [RouterLink, FormsModule, ReactiveFormsModule, CommonModule],
-  templateUrl: "./forgot-password.component.html",
-  styleUrl: "./forgot-password.component.scss",
+  selector: 'app-forgot-password',
+  imports: [RouterLink,FormsModule],
+  templateUrl: './forgot-password.component.html',
+  styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
-  forgotForm!: FormGroup;
-  public routes = routes;
-  constructor(private fb: FormBuilder, private router: Router) {
-    this.forgotForm = this.fb.group({
-      email: ["", [Validators.required, Validators.email]],
-    });
-  }
+  public routes = routes
+  constructor(private router: Router) {}
 
-  get email() {
-    return this.forgotForm.get("email")!;
-  }
-
-  onSubmit() {
-    console.log(this.forgotForm.value);
-
-    this.forgotForm.markAllAsTouched();
-    if (this.forgotForm.invalid) return;
-
+  public navigate() {
     this.router.navigate([routes.emailVerification]);
   }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,59 +1,25 @@
-import { Component } from "@angular/core";
-import { routes } from "../../shared/routes/routes";
-import { Router, RouterLink } from "@angular/router";
-import { CommonModule } from "@angular/common";
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from "@angular/forms";
+import { Component } from '@angular/core';
+import { routes } from '../../shared/routes/routes';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: "app-login",
-  imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
-  templateUrl: "./login.component.html",
-  styleUrl: "./login.component.scss",
+  selector: 'app-login',
+  imports: [CommonModule,RouterLink,FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginForm!: FormGroup;
-  hidePassword: boolean = true;
-  routes = routes;
-  constructor(private fb: FormBuilder, private router: Router) {
-    this.loginForm = this.fb.group({
-      email: ["", [Validators.required, Validators.email]],
-      password: ["", Validators.required],
-      rememberMe: [false],
-    });
-  }
+routes = routes;
+  constructor(private router: Router) {}
 
   public navigate() {
     this.router.navigate([routes.index]);
   }
+  public password : boolean[] = [false];
 
-  public togglePassword(index: any) {
-    this.hidePassword = !this.hidePassword;
-  }
-
-  onSubmit() {
-    // mark controls so errors show
-    console.log(this.loginForm.value);
-
-    this.loginForm.markAllAsTouched();
-
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    // this.router.navigate(["/"]);
-  }
-
-  // for template convenience
-  get email() {
-    return this.loginForm.get("email")!;
-  }
-  get password() {
-    return this.loginForm.get("password")!;
+  public togglePassword(index: any){
+    this.password[index] = !this.password[index]
   }
 }
