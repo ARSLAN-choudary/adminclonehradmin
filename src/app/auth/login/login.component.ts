cabb2fd2850d75109682429136658b55a9f432d0
Muhammad Fahad Khan <fahadkhan1229@gmail.com>
2025-07-26 15:49:06 +0500

    changing in data

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
-import { routes } from '../../shared/routes/routes';
-import { Router, RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {routes} from '../../shared/routes/routes';
+import {Router, RouterLink} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  imports: [CommonModule,RouterLink,FormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    imports: [CommonModule, RouterLink, FormsModule],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-routes = routes;
-  constructor(private router: Router) {}
+export class LoginComponent implements OnInit {
+    currentYear: any;
 
-  public navigate() {
-    this.router.navigate([routes.index]);
-  }
-  public password : boolean[] = [false];
+    ngOnInit() {
+        this.currentYear = new Date().getFullYear();
 
-  public togglePassword(index: any){
-    this.password[index] = !this.password[index]
-  }
+    }
+
+    routes = routes;
+
+    constructor(private router: Router) {
+    }
+
+    public navigate() {
+        this.router.navigate([routes.index]);
+    }
+
+    public password: boolean[] = [false];
+
+    public togglePassword(index: any) {
+        this.password[index] = !this.password[index]
+    }
 }
