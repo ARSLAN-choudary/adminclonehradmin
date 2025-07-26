ae83078d9475b839080d63c21037c0c6a3a50cc1
abrandhawa <alibakhat01@gmail.com>
2025-07-26 16:34:47 +0500

    login and forgot passoword forms created

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
-import { routes } from '../../shared/routes/routes';
-import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component } from "@angular/core";
+import { routes } from "../../shared/routes/routes";
+import { Router, RouterLink } from "@angular/router";
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-forgot-password',
-  imports: [RouterLink,FormsModule],
-  templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.scss'
+  selector: "app-forgot-password",
+  imports: [RouterLink, FormsModule, ReactiveFormsModule, CommonModule],
+  templateUrl: "./forgot-password.component.html",
+  styleUrl: "./forgot-password.component.scss",
 })
 export class ForgotPasswordComponent {
-  public routes = routes
-  constructor(private router: Router) {}
+  forgotForm!: FormGroup;
+  public routes = routes;
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.forgotForm = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+    });
+  }
 
-  public navigate() {
+  get email() {
+    return this.forgotForm.get("email")!;
+  }
+
+  onSubmit() {
+    console.log(this.forgotForm.value);
+
+    this.forgotForm.markAllAsTouched();
+    if (this.forgotForm.invalid) return;
+
     this.router.navigate([routes.emailVerification]);
   }
 }
