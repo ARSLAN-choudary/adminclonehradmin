754ea4d62bebb8425ada6a17564f87d62574e764
Muhammad Fahad Khan <fahadkhan1229@gmail.com>
2025-07-27 14:40:41 +0500

    changes

---
+++ src/app/Services/backend.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {CONFIG} from "../../config";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BackendService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    addCompany(parms: any): Observable<any> {
+        return this.http.post(CONFIG.addCompany, parms);
+    }
+}
