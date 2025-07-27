93a4e27b99ac3d57466706963284f277d1c433b7
abrandhawa <alibakhat01@gmail.com>
2025-07-27 22:48:20 +0500

    toaster added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,38 @@
-import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import {routes} from './app.routes';
-import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
-import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
-import {provideAnimations} from '@angular/platform-browser/animations';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {NgxMaskModule} from 'ngx-mask';
-import {JWT_OPTIONS, JwtHelperService} from '@auth0/angular-jwt';
-import {interceptorFn} from "./Services/interceptor.service";
+import { routes } from "./app.routes";
+import {
+  provideClientHydration,
+  withEventReplay,
+} from "@angular/platform-browser";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { NgxMaskModule } from "ngx-mask";
+import { JWT_OPTIONS, JwtHelperService } from "@auth0/angular-jwt";
+import { interceptorFn } from "./Services/interceptor.service";
+import { ToastrModule } from "ngx-toastr";
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        BsDatepickerModule.forRoot().providers!,
-        provideAnimations(),
-        provideHttpClient(),
-        provideHttpClient(withInterceptors([interceptorFn])),
-
-        {provide: JWT_OPTIONS, useValue: JWT_OPTIONS},
-        JwtHelperService,
-        NgxMaskModule.forRoot({
-            showMaskTyped: false,
-        }).providers!,
-    ],
-
+  providers: [
+    provideRouter(routes),
+    BsDatepickerModule.forRoot().providers!,
+    provideAnimations(),
+    provideHttpClient(),
+    provideHttpClient(withInterceptors([interceptorFn])),
+    importProvidersFrom(
+      ToastrModule.forRoot({
+        timeOut: 2000,
+        positionClass: "toast-top-right",
+        preventDuplicates: true,
+      })
+    ),
+    provideAnimations(),
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    JwtHelperService,
+    NgxMaskModule.forRoot({
+      showMaskTyped: false,
+    }).providers!,
+  ],
 };
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,95 +1,99 @@
-import {Component, OnInit, signal, WritableSignal} from "@angular/core";
-import {routes} from "../../shared/routes/routes";
-import {Router, RouterLink} from "@angular/router";
-import {CommonModule} from "@angular/common";
+import { Component, OnInit, signal, WritableSignal } from "@angular/core";
+import { routes } from "../../shared/routes/routes";
+import { Router, RouterLink } from "@angular/router";
+import { CommonModule } from "@angular/common";
 import {
-    FormBuilder,
-    FormGroup,
-    FormsModule,
-    ReactiveFormsModule,
-    Validators,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
 } from "@angular/forms";
-import {AuthService} from "../../Services/auth.service";
+import { AuthService } from "../../Services/auth.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-    selector: "app-login",
-    imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
+  selector: "app-login",
+  imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
 
-    templateUrl: "./login.component.html",
-    styleUrl: "./login.component.scss",
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.scss",
 })
 export class LoginComponent implements OnInit {
-    loginForm!: FormGroup;
-    hidePassword: boolean = true;
-    routes = routes;
-    errorMsg: WritableSignal<string> = signal("");
-    successMsg: WritableSignal<string> = signal("");
+  loginForm!: FormGroup;
+  hidePassword: boolean = true;
+  routes = routes;
+  errorMsg: WritableSignal<string> = signal("");
+  successMsg: WritableSignal<string> = signal("");
 
-    constructor(
-        private fb: FormBuilder,
-        private router: Router,
-        private auth: AuthService
-    ) {
-        this.loginForm = this.fb.group({
-            email: ["", [Validators.required]],
-            password: ["", Validators.required],
-        });
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private auth: AuthService,
+    private toastr: ToastrService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ["", [Validators.required]],
+      password: ["", Validators.required],
+    });
+  }
+
+  currentYear: any;
+
+  ngOnInit() {
+    this.currentYear = new Date().getFullYear();
+    this.checkIfAlreadyLogin();
+  }
+
+  checkIfAlreadyLogin() {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(["/"]);
+    }
+    localStorage.clear();
+  }
+
+  public navigate() {
+    this.router.navigate([routes.index]);
+  }
+
+  public togglePassword() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
     }
 
-    currentYear: any;
+    const { email, password } = this.loginForm.value;
 
-    ngOnInit() {
-        this.currentYear = new Date().getFullYear();
-        this.checkIfAlreadyLogin();
-    }
-
-    checkIfAlreadyLogin() {
-        if (this.auth.isLoggedIn()) {
-            this.router.navigate(["/"]);
+    this.auth.login(email, password).subscribe(
+      (data) => {
+        console.log(data);
+        if (data.data.token) {
+          localStorage.setItem("token", data.data.token);
+          this.toastr.success(data.message);
+          this.successMsg.set(data.message);
+          this.router.navigate(["/"]);
         }
-        localStorage.clear();
-    }
+      },
+      (error) => {
+        console.log(error.error.message);
+        this.errorMsg.set(error.error.message);
+        this.toastr.error(error.error.message);
+        setTimeout(() => {
+          this.errorMsg.set("");
+        }, 3000);
+      }
+    );
+  }
 
-    public navigate() {
-        this.router.navigate([routes.index]);
-    }
+  // for template convenience
+  get email() {
+    return this.loginForm.get("email")!;
+  }
 
-    public togglePassword() {
-        this.hidePassword = !this.hidePassword;
-    }
-
-    onSubmit() {
-        if (this.loginForm.invalid) {
-            return;
-        }
-
-        const {email, password} = this.loginForm.value;
-
-        this.auth.login(email, password).subscribe(
-            (data) => {
-                console.log(data);
-                if (data.data.token) {
-                    localStorage.setItem("token", data.data.token);
-                    this.successMsg.set(data.message);
-                    this.router.navigate(["/"]);
-                }
-            },
-            (error) => {
-                console.log(error.error.message);
-                this.errorMsg.set(error.error.message);
-                setTimeout(() => {
-                    this.errorMsg.set("");
-                }, 3000);
-            }
-        );
-    }
-
-    // for template convenience
-    get email() {
-        return this.loginForm.get("email")!;
-    }
-
-    get password() {
-        return this.loginForm.get("password")!;
-    }
+  get password() {
+    return this.loginForm.get("password")!;
+  }
 }
