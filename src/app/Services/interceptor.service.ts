93ec8e67536052a5ea531ee34df27ca5fd0e4f4a
abrandhawa <alibakhat01@gmail.com>
2025-07-28 19:30:36 +0500

    loader added

--- src/app/Services/interceptor.service.ts
+++ src/app/Services/interceptor.service.ts
@@ -1,28 +1,31 @@
 // interceptor.ts
-import {HttpInterceptorFn} from '@angular/common/http';
-import {inject} from '@angular/core';
-import {AuthService} from './auth.service';
-import {finalize, catchError, throwError} from 'rxjs';
+import { HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { finalize, catchError, throwError } from "rxjs";
+import { DataService } from "../shared/data/data.service";
 
 export const interceptorFn: HttpInterceptorFn = (req, next) => {
-    const authService = inject(AuthService);
-    const token = authService.getToken();
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+  const dataService = inject(DataService);
 
-    // spinner.show();
+  dataService.setLoaderState(true);
+  // spinner.show();
 
-    const isFormData = req.body instanceof FormData;
+  const isFormData = req.body instanceof FormData;
 
-    if (!req.url.includes('/login')) {
-        req = req.clone({
-            setHeaders: {
-                Authorization: `Bearer ${token}`,
-                ...(isFormData ? {} : {'Content-Type': 'application/json'}),
-            },
-        });
-    }
+  if (!req.url.includes("/login")) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+        ...(isFormData ? {} : { "Content-Type": "application/json" }),
+      },
+    });
+  }
 
-    return next(req).pipe(
-        // finalize(() => spinner.hide()),
-        catchError((error) => throwError(() => error))
-    );
+  return next(req).pipe(
+    finalize(() => dataService.setLoaderState(false)),
+    catchError((error) => throwError(() => error))
+  );
 };
