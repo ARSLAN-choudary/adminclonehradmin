02e1834d2bdd069c038d2710ae6455fc7adf0557
Muhammad Fahad Khan <fahadkhan1229@gmail.com>
2025-07-28 21:47:28 +0500

    new app

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,13 @@ const BASE_URL_API = "https://my-planent.onrender.com";
 // const BASE_URL_API = '';
 
 export const CONFIG = {
-  login: BASE_URL_API + "/api/auth/login",
-  addCompany: BASE_URL_API + "/api/company/addCompany",
-  getCompany: BASE_URL_API + "/api/company/getlist",
-  getManageUsers: BASE_URL_API + "/api/users/userslist",
-  addUser: BASE_URL_API + "/api/users/adduser",
-  updateUser: BASE_URL_API + "/api/users/updateuser",
-  deleteUser: BASE_URL_API + "/api/users/Delete",
-  addApplication: BASE_URL_API + "/api/application/add"
+    login: BASE_URL_API + "/api/auth/login",
+    addCompany: BASE_URL_API + "/api/company/addCompany",
+    getCompany: BASE_URL_API + "/api/company/getlist",
+    getManageUsers: BASE_URL_API + "/api/users/userslist",
+    addUser: BASE_URL_API + "/api/users/adduser",
+    updateUser: BASE_URL_API + "/api/users/updateuser",
+    deleteUser: BASE_URL_API + "/api/users/Delete",
+    addApplication: BASE_URL_API + "/api/application/add",
+    getApplications: BASE_URL_API + "/api/application/list",
 };
