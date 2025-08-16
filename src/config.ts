f85007f0c0da530553d4748f3cd73d7885d79e5c
Muhammad Fahad Khan <fahadkhan1229@gmail.com>
2025-08-16 16:07:52 +0500

    id related issue changes

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-const BASE_URL_API = "https://my-planent.onrender.com";
-// const BASE_URL_API = '';
+// const BASE_URL_API = "https://my-planent.onrender.com";
+const BASE_URL_API = '';
 
 export const CONFIG = {
   login: BASE_URL_API + "/api/auth/login",
