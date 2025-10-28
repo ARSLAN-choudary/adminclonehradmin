cb62efcfba4a80ca96f3f00e800a1cdde33f8028
Muhammad Fahad Khan <fahadkhan1229@gmail.com>
2025-10-28 21:20:06 +0500

    version added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,111 +1,111 @@
-import { Component, OnInit, signal, WritableSignal } from "@angular/core";
-import { routes } from "../../shared/routes/routes";
-import { Router, RouterLink } from "@angular/router";
-import { CommonModule } from "@angular/common";
+import {Component, OnInit, signal, WritableSignal} from "@angular/core";
+import {routes} from "../../shared/routes/routes";
+import {Router, RouterLink} from "@angular/router";
+import {CommonModule} from "@angular/common";
 import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
 } from "@angular/forms";
-import { AuthService } from "../../Services/auth.service";
-import { ToastrService } from "ngx-toastr";
-import { HttpErrorResponse } from "@angular/common/http";
+import {AuthService} from "../../Services/auth.service";
+import {ToastrService} from "ngx-toastr";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
-  selector: "app-login",
-  imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
+    selector: "app-login",
+    imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
 
-  templateUrl: "./login.component.html",
-  styleUrl: "./login.component.scss",
+    templateUrl: "./login.component.html",
+    styleUrl: "./login.component.scss",
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
-  hidePassword: boolean = true;
-  routes = routes;
-  errorMsg: WritableSignal<string> = signal("");
-  successMsg: WritableSignal<string> = signal("");
+    loginForm!: FormGroup;
+    hidePassword: boolean = true;
+    routes = routes;
+    errorMsg: WritableSignal<string> = signal("");
+    successMsg: WritableSignal<string> = signal("");
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private auth: AuthService,
-    private toastr: ToastrService
-  ) {
-    this.loginForm = this.fb.group({
-      email: ["", [Validators.required]],
-      password: ["", Validators.required],
-    });
-  }
-
-  currentYear: any;
-
-  ngOnInit() {
-    this.currentYear = new Date().getFullYear();
-    this.checkIfAlreadyLogin();
-  }
-
-  checkIfAlreadyLogin() {
-    if (this.auth.isLoggedIn()) {
-      this.router.navigate(["/"]);
-    }
-    localStorage.clear();
-  }
-
-  public navigate() {
-    this.router.navigate([routes.index]);
-  }
-
-  public togglePassword() {
-    this.hidePassword = !this.hidePassword;
-  }
-
-  onSubmit() {
-    if (this.loginForm.invalid) {
-      return;
+    constructor(
+        private fb: FormBuilder,
+        private router: Router,
+        private auth: AuthService,
+        private toastr: ToastrService
+    ) {
+        this.loginForm = this.fb.group({
+            email: ["", [Validators.required]],
+            password: ["", Validators.required],
+        });
     }
 
-    const { email, password } = this.loginForm.value;
+    currentYear: any;
 
-    this.auth.login(email, password).subscribe(
-      (data) => {
-        if (data.data.token) {
-          localStorage.setItem("token", data.data.token);
-          this.toastr.success(data.message);
-          this.successMsg.set(data.message);
-          this.router.navigate(["/"]);
+    ngOnInit() {
+        this.currentYear = new Date().getFullYear();
+        this.checkIfAlreadyLogin();
+    }
+
+    checkIfAlreadyLogin() {
+        if (this.auth.isLoggedIn()) {
+            this.router.navigate(["/"]);
         }
-      },
-      (error: HttpErrorResponse) => {
-        let backendMsg: string;
+        localStorage.clear();
+    }
 
-        if (
-          error.error &&
-          typeof error.error === "object" &&
-          "message" in error.error
-        ) {
-          backendMsg = (error.error as any).message;
-        } else if (typeof error.error === "string") {
-          backendMsg = error.error;
-        } else {
-          backendMsg = error.message;
+    public navigate() {
+        this.router.navigate([routes.index]);
+    }
+
+    public togglePassword() {
+        this.hidePassword = !this.hidePassword;
+    }
+
+    onSubmit() {
+        if (this.loginForm.invalid) {
+            return;
         }
 
-        this.errorMsg.set(backendMsg);
-        this.toastr.error(backendMsg);
+        const {email, password} = this.loginForm.value;
 
-        setTimeout(() => this.errorMsg.set(""), 3000);
-      }
-    );
-  }
+        this.auth.login(email, password).subscribe(
+            (data) => {
+                if (data.data.token) {
+                    localStorage.setItem("token", data.data.token);
+                    this.toastr.success(data.message);
+                    this.successMsg.set(data.message);
+                    this.router.navigate(["/"]);
+                }
+            },
+            (error: HttpErrorResponse) => {
+                let backendMsg: string;
 
-  // for template convenience
-  get email() {
-    return this.loginForm.get("email")!;
-  }
+                if (
+                    error.error &&
+                    typeof error.error === "object" &&
+                    "message" in error.error
+                ) {
+                    backendMsg = (error.error as any).message;
+                } else if (typeof error.error === "string") {
+                    backendMsg = error.error;
+                } else {
+                    backendMsg = error.message;
+                }
 
-  get password() {
-    return this.loginForm.get("password")!;
-  }
+                this.errorMsg.set(backendMsg);
+                this.toastr.error(backendMsg);
+
+                setTimeout(() => this.errorMsg.set(""), 3000);
+            }
+        );
+    }
+
+    // for template convenience
+    get email() {
+        return this.loginForm.get("email")!;
+    }
+
+    get password() {
+        return this.loginForm.get("password")!;
+    }
 }
