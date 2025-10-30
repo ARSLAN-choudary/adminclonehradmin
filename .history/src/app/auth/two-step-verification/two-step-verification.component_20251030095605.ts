d4014cc462783d607c9f4fd62872c290d5decfb5
faizan99904 <faizan99904@gmail.com>
2025-10-30 11:31:37 +0500

    changes

--- .history/src/app/auth/two-step-verification/two-step-verification.component_20251030095605.ts
+++ .history/src/app/auth/two-step-verification/two-step-verification.component_20251030095605.ts
@@ -47,7 +47,6 @@ export class TwoStepVerificationComponent implements OnInit, OnDestroy {
       this.fromApp = params['from'] === 'app'; // 🔹 Detect if opened from Flutter
       this.deviceId = params['deviceId'] || '';
       console.log('📩 Received email:', this.email, '| fromApp:', this.fromApp);
-      this.updateUrl()
     });
 
     this.startCountdown();
